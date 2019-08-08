49c8aa57bf2799fd8fb543e138d1fe45d7beac12
Brian J Weis <brian@brianweis.net>
2019-08-07 22:00:22 -0600

    Start of Cloud Functions

--- clound-funciton/main.py
+++ clound-funciton/main.py
@@ -1,31 +1,30 @@
 from aiohttp import web
 import asyncio, asyncssh, sys
 
-routes = web.RouteTableDef()
+async def foo():
+    return 'Asynchronicity! TWO'
 
-@routes.get('/')
-async def hello(request):
-    return web.Response(text="Hello, world")
+async def bar():
+    return await foo()
 
-@routes.get('/gogo')
-async def hello(request):
+
+async def hello():
     try:
         # asyncio.get_event_loop().run_until_complete(run_client())
         await run_client()
-        return web.Response(text="REturn")
+        return 'YAY'
     except (OSError, asyncssh.Error) as exc:
         sys.exit('SSH connection failed: ' + str(exc))
-        return web.Response(text="Lightup Fail")
-    
+        return 'NAY'
 
 async def run_client():
     print("RUNNING IT")
-    async with asyncssh.connect('75.71.20.33', 2222, username='pi', known_hosts=None, PASSWORD,) as conn:
+    async with asyncssh.connect('75.71.20.33', 2222, username='pi', known_hosts=None, password='DvyN4RG^s6',) as conn:
         result = await conn.run('python doorajar/switcher.py', check=True)
         #DvyN4RG^s6
         print(result.stdout, end='')
         return web.Response(text="Lightup")
 
-app = web.Application()
-app.add_routes(routes)
-web.run_app(app)
+def test(request):
+    # return null
+    return asyncio.run(hello())
